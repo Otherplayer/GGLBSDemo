745fbe385bdbb428a700f7c3fb65322956366662
zero <osmk@qq.com>
2015-08-02 15:31:21 +0800

    first commit

---
+++ GGLBSDemo/GGLBSManager.m
@@ -0,0 +1,13 @@
+//
+//  GGLBSManager.m
+//  GGLBSDemo
+//
+//  Created by __无邪_ on 15/7/31.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import "GGLBSManager.h"
+
+@implementation GGLBSManager
+
+@end
